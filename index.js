d1d8579ad06589ad603f477ff72166f9590b4a26
David Berkheimer <berkheimer112@atlanticbb.net>
2020-01-07 09:55:12 -0500

    added default file path

--- index.js
+++ index.js
@@ -58,8 +58,10 @@ ipcMain.on("year:add", (event, name) => {
   console.log(name);
   // close the addWindow
   addWindow.close();
-  // this is for extsions
+  // Set fileName to name
+  // filter for .deb extensions
   let myOptions = {
+    defaultPath: name,
     filters: [{ name: "Custom File Type", extensions: ["deb"] }]
   };
   // open save dialog to create a fileNamePath
